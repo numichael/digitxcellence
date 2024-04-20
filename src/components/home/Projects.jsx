1c2189c4f91c985a003caeba913dfb37b8b080d0
Lanzz <lanzzd191@gmail.com>
2024-04-20 09:25:32 +0100

    Something

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -58,12 +58,12 @@ const Project = ({ link, images, name, about}) => {
     return (
         <a href={link} target='_blank' className='cursor-pointer w-full lg:h-[45rem]'>
             <div className="relative w-full h-[80%] flex gap-2 lg:gap-4">
-                <div className="flex flex-col gap-2 lg:gap-4 w-[35%] h-full">
+                <div className="flex flex-col gap-2 lg:gap-4 w-[45%] lg:w-[35%] h-full">
                     <img style={image1Style} src={images[0]} ref={ref1} className='transition-all relative duration-1000 w-full h-full border' alt="Your Image" />
                 </div>
-                <div className="flex flex-col gap-2 lg:gap-4 w-[50%] h-full">
-                    <img style={image2Style} src={images[1]} ref={ref2} className='transition-all relative duration-1000 w-full border h-1/2' alt="Your Image" />
-                    <img style={image3Style} src={images[2]} ref={ref3} className='transition-all relative duration-1000 w-full border h-1/2' alt="Your Image" />
+                <div className="grid gap-2 lg:gap-4 w-[55%] lg:w-[50%]">
+                    <img style={image2Style} src={images[1]} ref={ref2} className='transition-all relative duration-1000 w-full border h-full' alt="Your Image" />
+                    <img style={image3Style} src={images[2]} ref={ref3} className='transition-all relative duration-1000 w-full border h-full' alt="Your Image" />
                 </div>
             </div>
             <div className="border-r-2 rounded-br-[2rem] py-4">
